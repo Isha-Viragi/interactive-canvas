c0bf3703108fa7d89eb02c5c3f3a66d2a176d629
Isha Viragi <isha.viragi@gmail.com>
2024-10-14 15:47:17 -0400

    Add color to shapes

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const c = canvas.getContext("2d");
 
 //Drawing practice
 //Rectangle
+c.fillStyle = "pink"
 c.fillRect(100, 100, 140, 100);
 
 //Line
@@ -17,9 +18,13 @@ c.moveTo(300, 100); //Initial point
 c.lineTo(400, 200); //Next point
 c.lineTo(400, 300); //Next point
 c.lineTo(600, 300); //Next point
+c.strokeStyle = "green"
 c.stroke(); //Fill line in
 
 //Circle
 c.beginPath(); //New line
 c.arc(500, 150, 30, 0, Math.PI * 2, false); //Create circle
+c.strokeStyle = "red"
 c.stroke(); //Fill line in
+c.fillStyle = "lightyellow"
+c.fill();
