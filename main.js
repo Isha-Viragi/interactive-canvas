b44c4f49d0d085c6e1c19c7f26e2dd2c72444302
Isha Viragi <isha.viragi@gmail.com>
2024-10-14 15:37:08 -0400

    Draw basic rectangle, line and circle on canvas

--- main.js
+++ main.js
@@ -7,4 +7,19 @@ canvas.height = window.innerHeight;
 //Set up context
 const c = canvas.getContext("2d");
 
+//Drawing practice
+//Rectangle
+c.fillRect(100, 100, 140, 100);
 
+//Line
+c.beginPath(); //New line
+c.moveTo(300, 100); //Initial point
+c.lineTo(400, 200); //Next point
+c.lineTo(400, 300); //Next point
+c.lineTo(600, 300); //Next point
+c.stroke(); //Fill line in
+
+//Circle
+c.beginPath(); //New line
+c.arc(500, 150, 30, 0, Math.PI * 2, false); //Create circle
+c.stroke(); //Fill line in
