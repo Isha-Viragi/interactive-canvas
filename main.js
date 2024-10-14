a05f361211dfd78dea600a28e25f997015e25c61
Isha Viragi <isha.viragi@gmail.com>
2024-10-14 15:18:50 -0400

    Finish initialization of canvas on viewport

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+//Set up canvas
 const canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+//Set up context
+const c = canvas.getContext("2d");
+
+
