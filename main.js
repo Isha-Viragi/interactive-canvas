6c5573a696f63cb21e1a90a974acbc6264786c99
Isha Viragi <isha.viragi@gmail.com>
2024-10-24 11:20:31 -0400

    Clear screen after every incrementation

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let y = 100;
 
 function animate() {
   requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
 
   c.beginPath();
   c.arc(x, y, 30, 0, Math.PI * 2, false);
