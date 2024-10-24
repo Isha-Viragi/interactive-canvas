eb821810607ae3f147cdb0f886ffe97437122096
Isha Viragi <isha.viragi@gmail.com>
2024-10-24 10:59:01 -0400

    Draw a red circle on screen

--- main.js
+++ main.js
@@ -7,4 +7,8 @@ canvas.height = window.innerHeight;
 //Set up context
 const c = canvas.getContext("2d");
 
+c.beginPath();
+c.arc(100, 100, 30, 0, Math.PI * 2, false);
+c.strokeStyle = "red";
+c.stroke();
 
