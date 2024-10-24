d3867666ef79aa6365d604cdac4fbf6ca69056e9
Isha Viragi <isha.viragi@gmail.com>
2024-10-24 11:04:25 -0400

    Move circle creation code inside recursive animate function

--- main.js
+++ main.js
@@ -7,14 +7,16 @@ canvas.height = window.innerHeight;
 //Set up context
 const c = canvas.getContext("2d");
 
-c.beginPath();
-c.arc(100, 100, 30, 0, Math.PI * 2, false);
-c.strokeStyle = "red";
-c.stroke();
+
 
 function animate() {
   requestAnimationFrame(animate);
-  console.log('test');
+
+  c.beginPath();
+  c.arc(100, 100, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "red";
+  c.stroke();
+
 }
 
 animate();
