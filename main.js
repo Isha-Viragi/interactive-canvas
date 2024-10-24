c9db92dd1e27b1d18652f062dc9dbcc5e8ea600d
Isha Viragi <isha.viragi@gmail.com>
2024-10-24 11:18:36 -0400

    Move x y coordinates of circle drawing through recurssion and incrementation

--- main.js
+++ main.js
@@ -7,15 +7,20 @@ canvas.height = window.innerHeight;
 //Set up context
 const c = canvas.getContext("2d");
 
+let x = 100;
+let y = 100;
 
 
 function animate() {
   requestAnimationFrame(animate);
 
   c.beginPath();
-  c.arc(100, 100, 30, 0, Math.PI * 2, false);
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
   c.strokeStyle = "red";
   c.stroke();
+
+  x++;
+  y++;
 
 }
 
