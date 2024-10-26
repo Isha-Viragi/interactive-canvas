dd6c971be60e2ac9afb5f7ad4858dfc23f61d0f1
Isha Viragi <isha.viragi@gmail.com>
2024-10-26 11:28:42 -0400

    Merge multiple-objects-animation-practice branch + Increase radius of circles that come in contact with cursor

--- main.js
+++ main.js
@@ -7,4 +7,72 @@ canvas.height = window.innerHeight;
 //Set up context
 const c = canvas.getContext("2d");
 
+const mouse = {
+  x: undefined,
+  y: undefined
+}
 
+//Event Listener
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse)
+})
+
+//Circle class
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = () => {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "red";
+    c.stroke();
+  }
+
+  this.update = () => {
+    this.draw();
+    //Interactivity
+    if ((this.x > mouse.x - 30 && this.x < mouse.x + 30) &&
+      (this.y > mouse.y - 30 && this.y < mouse.y + 30)) {
+      this.radius += 2;
+    }
+
+
+    //Animate
+    if (this.x + this.radius >= window.innerWidth || this.x - this.radius <= 0)
+      this.dx *= -1;
+    if (this.y + this.radius >= window.innerHeight || this.y - this.radius <= 0)
+      this.dy *= -1;
+
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+}
+
+//Create array of circles
+const circleArray = [];
+
+for (let i = 0; i < 200; i++) {
+  let radius = 5;
+  let x = Math.random() * (window.innerWidth - radius * 2) + radius;
+  let y = Math.random() * (window.innerHeight - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 2;
+  let dy = (Math.random() - 0.5) * 2;
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update()
+  }
+}
+
+animate();
