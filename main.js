b63989a635275707f9200ea0f0da4e27c749935e
Isha Viragi <isha.viragi@gmail.com>
2024-10-26 10:02:28 -0400

    Prevent Circles spawning in window edges by adjusting x y coordinates with radius

--- main.js
+++ main.js
@@ -38,12 +38,12 @@ function Circle(x, y, dx, dy, radius) {
 //Create array of circles
 const circleArray = [];
 
-for (let i = 0; i < 3; i++) {
-  let x = Math.random() * window.innerWidth;
-  let y = Math.random() * window.innerHeight;
-  let dx = (Math.random() - 0.5) * 8;
-  let dy = (Math.random() - 0.5) * 8;
+for (let i = 0; i < 200; i++) {
   let radius = 30;
+  let x = Math.random() * (window.innerWidth - radius * 2) + radius;
+  let y = Math.random() * (window.innerHeight - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 2;
+  let dy = (Math.random() - 0.5) * 2;
   circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
